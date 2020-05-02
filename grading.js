6a9044da37409c7edd37d26b0c4d0b0479205da3
Rines.io <tsrines@gmail.com>
2020-05-02 16:04:22 -0400

    first commit

---
+++ grading.js
@@ -0,0 +1,16 @@
+
+// https://www.hackerrank.com/challenges/grading/problem
+const grades = [84,23,58,35,100,99,78,88,98,48]
+function gradingStudents(grades) {
+    // Write your code here
+    const newGrades = grades.map(grade => {
+        if (grade > 37 && (grade % 5) > 2) return grade += 5 - grade%5
+          else return grade
+    })
+    console.log(newGrades)
+    return newGrades
+
+}
+
+gradingStudents(grades)
+
